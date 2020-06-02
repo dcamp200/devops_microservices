fe936e634b997aca3a0f9eabddc8b84a89987a01
david <dcamp200@gmail.com>
2020-06-02 17:03:50 -0400

    Completed Task 4. Added docker upload commands

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,7 +5,7 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=dcamp200/predict
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +13,9 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+# Authenticate & Tag
+docker login &&\
+    docker image tag predict $dockerpath
+
+# Push Image
+docker image push $dockerpath 
