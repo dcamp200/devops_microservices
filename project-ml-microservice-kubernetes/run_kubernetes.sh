d7ace7162777f746f85332e9a22b11d0e63e749b
david <dcamp200@gmail.com>
2020-06-02 18:51:48 -0400

    Completed Task 6. Added kubernetes commands

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="dcamp200/predict"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run predict\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=predict
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward predict 8000:80
